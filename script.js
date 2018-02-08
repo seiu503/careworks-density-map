93ec9372b608e02f0c3295cc60386d4442f26aea
rifkegribenes <rifkegribenes@gmail.com>
2018-02-08 12:55:42 -0800

    legend show/hide working

--- script.js
+++ script.js
@@ -57,8 +57,68 @@ d3.queue()
       .attr('cy', d => projection([d[7], d[6]])[1])
       .attr("r",  10)
       .attr("id", d => `id${d[0]}`)
+      .attr("class", (d) => `circle circle-${d[5]}`)
       .attr('fill', d => colorScale(d[5]))
       .style('opacity', d => opacityScale(d[5]));
+
+  const legendRectSize = 18;
+  const legendSpacing = 8;
+
+  const legend = svg.selectAll('.legend')
+    .data(colorScale.domain())
+    .enter()
+    .append('g')
+    .attr('class', 'legend')
+    .attr('id', (d) => d)
+    .attr('transform', function(d, i) {
+      var h = legendRectSize + legendSpacing;
+      var offset =  h * colorScale.domain().length / 2;
+      let horz = 30 + (i * width / 6);
+      return 'translate(' + horz + ',' + h + ')';
+    })
+    .on('click', (d) => {
+      const allCircles = Array.from(document.getElementsByClassName('circle'));
+      const otherAssessments = allCircles.filter(el => !el.classList.contains(`circle-${d}`));
+      console.log(`${d}: otherAssessments`);
+      console.log(otherAssessments);
+      const assessmentMatches = Array.from(document.getElementsByClassName(`circle-${d}`));
+      console.log(`${d}: assessmentMatches`);
+      console.log(assessmentMatches);
+      otherAssessments.forEach((el) => {
+        el.classList.remove('visible');
+        el.classList.add('hidden');
+      });
+      assessmentMatches.forEach((el) => {
+        el.classList.remove('hidden');
+        el.classList.add('visible');
+      });
+      document.getElementById('btn').classList.add('btn-show');
+      document.getElementById('btn').classList.remove('btn-hide');
+    });
+
+legend.append('circle')
+  .attr('r', legendRectSize / 2)
+  .style('fill', colorScale)
+  .style('stroke', colorScale);
+
+legend.append('text')
+  .attr('x', legendRectSize )
+  .attr('y', legendRectSize - (legendSpacing * 1.5))
+  .text(function(d) { return d; });
+
+const showAll = () => {
+  const allCircles = Array.from(document.getElementsByClassName('circle'));
+  allCircles.forEach((el) => {
+    el.classList.remove('hidden');
+    el.classList.add('visible');
+  });
+  document.getElementById('btn').classList.add('btn-hide');
+  document.getElementById('btn').classList.remove('btn-show');
+  document.getElementById('button-wrap').classList.add('btn-hide');
+  document.getElementById('button-wrap').classList.remove('btn-show');
+}
+
+document.getElementById('btn').addEventListener("click", showAll);
 
 });
 
