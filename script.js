8a508e67680fb3ae9d69195e5f8aac63c3d5d268
rifkegribenes <rifkegribenes@gmail.com>
2018-02-08 12:36:37 -0800

    color and opacity scales fixed

--- script.js
+++ script.js
@@ -1,138 +1,73 @@
-const initialWidth = window.innerWidth;
-let width = initialWidth;
-let height = width / 1.4;
-const tooltip = d3.select('body').append('div').attr('class', 'tooltip').attr('id', 'tip');
-const tip = document.getElementById('tip');
+let width = window.innerWidth;
+let height = window.innerHeight;
 const contactScale = d3.scaleLinear().domain([1,2,3]);
 const colorScale = d3.scaleLinear().domain([1,2,3]);
+const opacityScale = d3.scaleLinear().domain([1,2,3]);
 
+var projection = d3.geoCylindricalEqualArea().parallel([45]);
 
-  var projection = d3.geoCylindricalEqualArea().parallel([45]);
+var path = d3.geoPath()
+    .projection(projection);
 
-  var path = d3.geoPath()
-      .projection(projection);
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class", "map");
 
-  var svg = d3.select("body").append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("class", "map");
+d3.queue()
+  .defer(d3.json, 'https://raw.githubusercontent.com/seiu503/careworks-density-map/master/oregon-counties.json?token=AX1nvcyQFzmMOxqYxLDsRngZNFVFt0fiks5ahdiIwA%3D%3D')
+  .defer(d3.json, 'https://raw.githubusercontent.com/seiu503/careworks-density-map/master/cw.json?token=AX1nvY85DaeJj9L23zPSgCOwzb2Em0nVks5ahLVSwA%3D%3D')
+  .await((error, or, contacts) => {
 
-  d3.queue()
-    .defer(d3.json, 'https://raw.githubusercontent.com/seiu503/careworks-density-map/master/oregon-counties.json?token=AX1nvcyQFzmMOxqYxLDsRngZNFVFt0fiks5ahdiIwA%3D%3D')
-    .defer(d3.json, 'https://raw.githubusercontent.com/seiu503/careworks-density-map/master/cw.json?token=AX1nvY85DaeJj9L23zPSgCOwzb2Em0nVks5ahLVSwA%3D%3D')
-    .await((error, or, contacts) => {
+  if (error) console.log(error);
 
-    if (error) console.log(error);
+  var counties = topojson.feature(or, or.objects.cb_2015_oregon_county_20m);
 
-    // console.log(or.objects.cb_2015_oregon_county_20m);
+  projection.scale(1)
+    .translate([0, 0]);
 
-    var counties = topojson.feature(or, or.objects.cb_2015_oregon_county_20m);
+  var b = path.bounds(counties),
+    s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
+    t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
 
-    projection.scale(1)
-      .translate([0, 0]);
+  projection.scale(s)
+    .translate(t);
 
-    var b = path.bounds(counties),
-      s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
-      t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+  svg.append("g")
+    .attr("class", "counties")
+    .selectAll("path")
+    .data(topojson.feature(or, or.objects.cb_2015_oregon_county_20m).features)
+    .enter().append("path")
+    .attr("d", path);
 
-    projection.scale(s)
-      .translate(t);
+  contactScale
+    .range([1, 2, 3]);
 
-    svg.append("g")
-      .attr("class", "counties")
-      .selectAll("path")
-      .data(topojson.feature(or, or.objects.cb_2015_oregon_county_20m).features)
-      .enter().append("path")
-      .attr("d", path);
+  colorScale
+    .range(["#ff0000", "#ff8000", "#ffff00"]);
+  opacityScale
+    .range([1, .6, .3]);
 
-  // svg.append("path")
-  //     .attr("class", "county-borders")
-  //     .attr("d", path(topojson.mesh(
-  //       or, or.objects.cb_2015_oregon_county_20m,
-  //       (a, b) => a !== b
-  //       )
-  //     ));
-
-
-    // svg.append("path")
-    //     .datum(topojson.mesh(or, or.features.counties, function(a, b) { return a !== b; }))
-    //     .attr("class", "mesh")
-    //     .attr("d", path);
-
-
-    // svg.append("path")
-    //     .data(counties)
-    //     .attr("class", "outline")
-    //     .attr("d", path)
-    //     .attr('id', 'land');
-
-
-     // svg.append("clipPath")
-     //    .attr("id", "clip-land")
-     //    .append("use")
-     //    .attr("xlink:href", "#land");
-        // .attr("transform", transform(d3.zoomIdentity));
-
-    // svg.selectAll("path")
-    //     .data(topojson.feature(or, or.features.counties).features)
-    //     .enter().append("path")
-    //     .attr("d", path)
-    //     .attr('county-id', function(d){
-    //        return d.id
-    //     }).attr("clip-path", "url(#clip-land)")
-    //     .attr('class', 'county');
-        // .attr("transform", transform(d3.zoomIdentity));
-
-    contactScale
-      .range([1, 2, 3]);
-
-    colorScale
-      .range(["#CC0000", "#E68000", "#FFFF66"]);
-
-    svg.append('g')
-      .selectAll('.contact')
-      .data(contacts)
-      .enter().append('circle')
-        .attr('class', 'contact')
-        .attr('cx', d => projection([d[7], d[6]])[0])
-        .attr('cy', d => projection([d[7], d[6]])[1])
-        .attr("r",  10)
-        .attr("id", d => `id${d[0]}`)
-        .attr('fill', d => colorScale(d[5]))
-        .style('opacity', '0.5')
-        .attr("transform", d3.zoomIdentity)
-        .on('mouseover', (d) => {
-          tooltip.transition()
-            .duration(100)
-            .style('opacity', .9);
-          tooltip.html(`<span class="tip-name">${d[5]}</span>`) // assessment
-            .style('left', `${d3.event.pageX}px`)
-            .style('zIndex', 3)
-            // keep tooltips from overlapping with circles
-            .style('top', `${d3.event.pageY + 20}px`);
-        })
-        .on('mouseout', () => {
-          tooltip.transition()
-          .duration(400)
-          .style('opacity', 0);
-        });
-        ;
+  svg.append('g')
+    .selectAll('.contact')
+    .data(contacts)
+    .enter().append('circle')
+      .attr('class', 'contact')
+      .attr('cx', d => projection([d[7], d[6]])[0])
+      .attr('cy', d => projection([d[7], d[6]])[1])
+      .attr("r",  10)
+      .attr("id", d => `id${d[0]}`)
+      .attr('fill', d => colorScale(d[5]))
+      .style('opacity', d => opacityScale(d[5]));
 
 });
 
-// const transform = (t) => {
-//   return (d) => {
-//     return "translate(" + t.apply(d) + ")";
-//   };
-// }
-
 const zoom = d3.zoom()
-  .scaleExtent([0.5, 10])
+  .scaleExtent([0.5, 20])
   .on('zoom', () => {
-    svg.attr("transform", d3.event.transform);
     d3.selectAll('g').attr("transform", d3.event.transform);
-    d3.selectAll('path').attr("transform", d3.event.transform);
-    d3.selectAll('clipPath').attr("transform", d3.event.transform);
+    d3.selectAll("circle")
+      .attr("r", (10 / d3.event.transform.k));
   });
 
 d3.select("body").call(zoom);
