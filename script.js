bc7c7cc9da0be236b8f7415bc47abadb81b0cd3e
rifkegribenes <rifkegribenes@gmail.com>
2018-02-07 17:09:30 -0800

    delete extra datafiles"

--- script.js
+++ script.js
@@ -7,32 +7,11 @@ let height = width / 1.4;
 
 // const colorScale = d3.scaleLinear().domain([1,2,3]);
 
-// const svg = d3.select('body').append('svg')
-//   .attr('width', width)
-//   .attr('height', height);
-
-// const projection = d3.geoAlbersUsa()
-//   .scale(1)
-//   .translate([0, 0]);
-
 // const path = d3.geoPath().projection(projection);
 
 // const borders = svg.append("g")
 //   .attr('class', 'borders');
 
-// const resize = () => {
-//   let width = window.innerWidth;
-//   let height = width / 2;
-//   svg.attr('width', width).attr('height', height);
-//   projection.scale(width / 2 / Math.PI).translate([width / 2, height / 2]);
-//   d3.select("g").attr("transform", `scale(${width / initialWidth})`);
-//   d3.selectAll("circle")
-//     .attr('cx', d => projection([d[6], d[7]]))
-//     .attr('cy', d => projection([d[6], d[7]]));
-// }
-
-  // var width = 960,
-  //   height = 500;
 
   var projection = d3.geoAlbers();
 
@@ -165,4 +144,3 @@ const zoom = d3.zoom()
   .on('zoom', () => d3.selectAll('g').attr('transform', d3.event.transform));
 
 svg.call(zoom);
-// d3.select(window).on("resize", resize);
